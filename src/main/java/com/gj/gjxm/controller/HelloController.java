5f34c398366c02015ed1da7dd557544ed9896838
lijunlin <junlinok@163.com>
2020-11-22 19:06:11 +0800

    change return

--- src/main/java/com/gj/gjxm/controller/HelloController.java
+++ src/main/java/com/gj/gjxm/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello springboot";
+        return "hello springboot git";
     }
 }
